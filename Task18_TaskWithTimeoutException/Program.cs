574a70380c8137927dfa0611783ca9262fdc0bd6
Hasanov Agil <47672788+hsnvagil@users.noreply.github.com>
2024-09-26 17:45:43 +0400

    Update Program.cs

--- Task18_TaskWithTimeoutException/Program.cs
+++ Task18_TaskWithTimeoutException/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            int timeout = 3000;
+            var task = SomeLongRunningTask();
+            var timeoutTask = Task.Delay(timeout);
+
+            var completedTask = await Task.WhenAny(task, timeoutTask);
+
+            if (completedTask == timeoutTask)
+            {
+                throw new TimeoutException("Task təyin olunan zaman ərzində tamamlanmadı.");
+            }
+
+            Console.WriteLine("Task nəticəsi: " + await task);
+        }
+        catch (TimeoutException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static async Task<string> SomeLongRunningTask()
+    {
+        await Task.Delay(5000);
+        return "Task tamamlandı!";
+    }
+}
